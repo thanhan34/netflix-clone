ebc2c295b6d5fbc70359ecc5929ec356421b8a88
An Doan <dtan42@gmail.com>
2020-09-25 13:16:22 +1000

    Loading poster and thumnail image on the row component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import Row from "./Row";
 import './App.css';
+import requests from "./requests";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello An Doan
-        
-      </h1>
-        
+      <h1>Hello An Doan</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow />
+      <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Moives" fetchUrl={requests.fetchHorrorMoives} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
